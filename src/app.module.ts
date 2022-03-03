ab7795e4091c6f77b75b34b9065d3f0913c00233
Hilal ahmad <86137247+hilalahmad32@users.noreply.github.com>
2022-03-03 05:06:29 -0800

    change in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, MongooseModule.forRoot("mongodb+srv://hilalahmad:hilalahmad@cluster0.fzn2l.mongodb.net/nestjs_sveltejs?retryWrites=true&w=majority"), ProductModule],
+  imports: [AuthModule, MongooseModule.forRoot(""), ProductModule],
 })
 export class AppModule { }
